b8d803bf42f1c7d7e9d3453e51f79cd8bf0a0d7e
Marco Torquati <m.torquati@GOBID.LOCAL>
2024-12-13 16:21:01 +0100

    seconda implementazione - fino a 'logging in'

--- DJANGO/core/views.py
+++ DJANGO/core/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from item.models import Category, Item
 
+from .forms import SignupForm
 
 
 """"
@@ -45,3 +46,18 @@ Questa pagina può contenere un modulo per l'invio di messaggi, informazioni di 
 def contact(request):
     return render(request, 'core/contact.html')
 
+
+def signup(request):
+    if request.method == 'POST':
+        form =SignupForm(request.POST)
+        
+        if form.is_valid():
+            form.save()
+            
+            return redirect ('/login/')
+    else:
+        form= SignupForm()
+
+    return render(request, 'core/signup.html', {
+        'form': form
+    })
